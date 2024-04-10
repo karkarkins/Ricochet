f769a390185f34709e43f76a40e897d802450e1d
karkarkins <dakarailanes@gmail.com>
2024-04-10 03:18:13 -0400

    Laser Quality Improved
    Laser sprite swapped to line renderer
    Laser follows mouse position
    Laser is interfered with by colliders

--- Ricochet/Assets/Scripts/PlayerController.cs
+++ Ricochet/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     float angle;
     public GameObject projectilePrefab;
-    public GameObject laser;
+
+    public LineRenderer lineRenderer;
+    public Transform firePoint;
     // Start is called before the first frame update
     void Start()
     {
-        //Vector2 laserOffset = new Vector2(transform.position.x + 0.46f, transform.position.y + 0.08f);
-        //laser.transform.position = laserOffset;
-        Instantiate(laser, transform);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Gun tracks mouse and points to its location
         Vector3 mouseScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z);
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPos);
         angle = Mathf.Atan2(mouseScreenPos.y, mouseScreenPos.x) * Mathf.Rad2Deg;
@@ -27,6 +28,23 @@ public class PlayerController : MonoBehaviour
         {
             Instantiate(projectilePrefab, transform.position, transform.rotation);
         }
+        UpdateLaser();
+    }
 
+    void UpdateLaser()
+    {
+        Vector2 mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        lineRenderer.SetPosition(0, firePoint.position);
+
+        lineRenderer.SetPosition(1, mousePos);
+
+        Vector2 direction = mousePos - (Vector2)transform.position;
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, direction.normalized, direction.magnitude);
+
+        if (hit)
+        {
+            lineRenderer.SetPosition(1, hit.point);
+        }
     }
 }
