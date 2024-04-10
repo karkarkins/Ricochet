e7e2942b56501ece20c0c95348cc88032eabb5f9
karkarkins <dakarailanes@gmail.com>
2024-04-10 00:08:13 -0400

    Major Commit
    Added TileMap
    Created Level
    Created Enemy, Player, Bullet, and Gun Prefabs
    Created script for gun sprite to follow the mouse
    Created script for bullet instantiation on left click
    Created script for bullet movement to the right of screen

---
+++ Ricochet/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouseScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z);
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        angle = Mathf.Atan2(mouseScreenPos.y, mouseScreenPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
---
+++ Ricochet/Assets/Scripts/Shoot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
